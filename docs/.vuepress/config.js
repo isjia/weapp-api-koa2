05134287a243aa6868bec95c6d07a647b9c8ef5d
Jenny Qian <isjia@163.com>
2019-07-09 14:21:56 +0800

    init notes

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Node.js Koa2 实现微信小程序 api',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Notes', link: '/notes/' },
+      { text: 'API文档', link: 'http://www.7yue.pro/#/home' },
+    ],
+    sidebar: [
+      {
+        title: 'Notes',
+        collapsable: false,
+        children: [
+          '/notes/ch01',
+        ]
+      }
+    ]
+  }
+}
