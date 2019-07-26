11bbfe4f79abaa19050aad2f73cd47fb69757020
Jenny Qian <isjia@163.com>
2019-07-26 15:26:50 +0800

    finish ch06-03

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
+const bcrypt = require('bcryptjs');
+
 const router = new Router({
   prefix: '/v1/user',
 });
@@ -13,10 +15,11 @@ router.post('/register', async (ctx) => {
   // 1. api 需要接受的参数
   // 2. 参数校验
   const v = await new RegisterValidator().validate(ctx);
-
+  const salt = bcrypt.genSaltSync(10);
+  const pwd = bcrypt.hashSync(v.get('body.password1'), salt);
   const user = {
     email: v.get('body.email'),
-    password: v.get('body.password1'),
+    password: pwd,
     nickname: v.get('body.nickname'),
   }
 
