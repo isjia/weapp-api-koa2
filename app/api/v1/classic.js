81faab03d1396a3be4447553012d437e89caec63
Jenny Qian <isjia@163.com>
2019-07-30 14:56:44 +0800

    finish ch09-07

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -5,6 +5,8 @@ const { Auth } = require('../../../middlewares/auth');
 const { Flow } = require('../../models/flow');
 const { Art } = require('../../models/art');
 const { Favor } = require('@models/favor');
+const { PositiveIntegerValidator } = require('@validators/validator');
+const { NotFoundException } = require('@core/http-exception');
 
 const router = new Router({
   prefix: '/v1/classic',
@@ -23,5 +25,55 @@ router.get('/latest', new Auth().m, async (ctx, next) => {
   art.setDataValue('like_status', favor);
   ctx.body = art;
 });
+
+router.get('/:index/next', new Auth().m, async (ctx, next) => {
+  const v = await new PositiveIntegerValidator().validate(ctx, {
+    id: 'index'
+  });
+
+  const index = v.get('path.index');
+
+  const flow = await Flow.findOne({
+    where: {
+      index: index + 1
+    }
+  });
+
+  if (!flow) {
+    throw new NotFoundException('没有找到下一期期刊');
+  }
+
+  // 返回下一期期刊的详情
+  const art = await Art.getData(flow.art_id, flow.type);
+  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid);
+  art.setDataValue('index', flow.index);
+  art.setDataValue('like_status', favor);
+  ctx.body = art;
+})
+
+router.get('/:index/previous', new Auth().m, async (ctx, next) => {
+  const v = await new PositiveIntegerValidator().validate(ctx, {
+    id: 'index'
+  });
+
+  const index = v.get('path.index');
+
+  const flow = await Flow.findOne({
+    where: {
+      index: index - 1
+    }
+  });
+
+  if (!flow) {
+    throw new NotFoundException('没有找到上一期期刊');
+  }
+
+  // 返回下一期期刊的详情
+  const art = await Art.getData(flow.art_id, flow.type);
+  const favor = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid);
+  art.setDataValue('index', flow.index);
+  art.setDataValue('like_status', favor);
+  ctx.body = art;
+})
 
 module.exports = router;
